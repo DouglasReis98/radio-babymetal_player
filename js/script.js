b891f1d4a6b2295b675a6646e72fcb12edef1837
DouglasReis <douglasamaralreis@outlook.com>
2023-02-12 11:42:14 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
-const btnPlay = document.getElementById("play");
-const btnPause = document.getElementById("pause");
-const btnStop = document.getElementById("stop");
+const player = document.getElementById("player");
+const btnPlay = document.getElementById("btnPlay");
+const btnPause = document.getElementById("btnPause");
+const btnStop = document.getElementById("btnStop");
+const ctrlVol = document.getElementById("ctrlVol");
 const btnVol = document.getElementById("btnVol");
+console.log(btnPlay)
+
+
+btnPlay.addEventListener("click", function() {
+    player.play();
+});
+
+btnPause.addEventListener("click", function() {
+    player.pause();
+});
+
+btnStop.addEventListener("click", function() {
+    player.pause();
+    player.currentTime = 0;
+    player.load();
+});
+
+ctrlVol.addEventListener("input", function() {
+    player.volume = ctrlVol.value;
+});
+
+btnVol.addEventListener("click", function() {
+    if (player.muted != true) {
+        player.muted = true;
+    } else {
+        player.muted = false;
+    }
+
+});
